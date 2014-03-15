c57b1bff1b635799c5d7d7bcd4e6329f225b7810
Todd Eddy <vr@vrillusions.com>
2014-03-14 22:36:11 -0400

    First cleanup pass. Ref gh-6. Closes gh-2

--- update_twitter.py
+++ update_twitter.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python
-# vim:ts=4:sw=4:ft=python:fileencoding=utf-8
-"""Python Template.
+# vim: set fileencoding=utf-8 :
+"""Update twitter from an rss feed.
 
-This is a template for python.
+Parse an rss feed for new entries and post them to twitter.
+
+Environment Variables
+    LOGLEVEL: overrides the level specified here. Default is warning
 
 """
 
-
-import sys
+from __future__ import (division, absolute_import, print_function,
+                        unicode_literals)
+import logging
 import os
-import traceback
+import sys
 import cPickle
 from ConfigParser import SafeConfigParser
 
@@ -18,31 +22,46 @@ sys.path.append("./lib/tweepy")
 import tweepy
 
 
-__version__ = '0.2.0'
+__version__ = '0.3.0'
+
+
+# Logger config
+# DEBUG, INFO, WARNING, ERROR, or CRITICAL
+# This will set log level from the environment variable LOGLEVEL or default
+# to warning. You can also just hardcode the error if this is simple.
+_LOGLEVEL = getattr(logging, os.getenv('LOGLEVEL', 'WARNING').upper())
+_LOGFORMAT = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+logging.basicConfig(level=_LOGLEVEL, format=_LOGFORMAT)
 
 
 def post_update(status):
+    log = logging.getLogger('post_update')
+    log.debug('status: {}'.format(status))
     config = SafeConfigParser()
     if not config.read('config.ini'):
-        print 'Could not read config file'
+        log.critical('Could not read config file')
         sys.exit(1)
     consumer_key = config.get('twitter', 'consumer_key')
     consumer_secret = config.get('twitter', 'consumer_secret')
     access_token = config.get('twitter', 'access_token')
     access_token_secret = config.get('twitter', 'access_token_secret')
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret, secure=True)
     auth.set_access_token(access_token, access_token_secret)
     api = tweepy.API(auth)
     try:
         api.update_status(status)
-    except tweepy.error.TweepError, e:
-        print "Error occurred while updating status:", e
+    except tweepy.error.TweepError as exc:
+        log.critical('Error occurred while updating status: {}'.format(exc))
         sys.exit(1)
     else:
         return True
 
-        
+
 def process_feed(url, cache_file, suffix = '#xkcd'):
+    log = logging.getLogger('process_feed')
+    log.debug('url: {}'.format(url))
+    log.debug('cache_file: {}'.format(cache_file))
+    log.debug('suffix: {}'.format(suffix))
     feed = feedparser.parse(url)
     # lots of scary warnings about possible security risk using this method
     # but for local use I'd rather do this than a try-catch with open()
@@ -57,10 +76,11 @@ def process_feed(url, cache_file, suffix = '#xkcd'):
     rss['summary'] = feed['entries'][0]['summary']
     # compare with cache
     if cache['id'] != rss['id']:
-        #print 'new post %s %s' % (rss['title'], rss['link'])
-        post_update('%s %s %s' % (rss['title'], rss['link'], suffix))
+        post = '{} {} {}'.format(rss['title'], rss['link'], suffix)
+        log.info('new post: {}'.format(post))
+        post_update(post)
         cPickle.dump(rss, open(cache_file, 'wb'), -1)
- 
+
 
 def main():
     """The main function."""
@@ -70,21 +90,4 @@ def main():
 
 
 if __name__ == "__main__":
-    try:
-        main()
-    except KeyboardInterrupt, e:
-        # Ctrl-c
-        raise e
-    except SystemExit, e:
-        # sys.exit()
-        raise e
-    except Exception, e:
-        print "ERROR, UNEXPECTED EXCEPTION"
-        print str(e)
-        traceback.print_exc()
-        sys.exit(1)
-    else:
-        # Main function is done, exit cleanly
-        sys.exit(0)
-
-
+    sys.exit(main())
