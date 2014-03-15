dbce45e89a2fb3609974d5769241bca64e2c99d4
Todd Eddy <vr@vrillusions.com>
2014-03-15 00:19:00 -0400

    Finish cleanup of code and update libraries
    Closes: gh-3
    Closes: gh-5
    Closes: gh-6

--- update_twitter.py
+++ update_twitter.py
@@ -16,6 +16,7 @@ import os
 import sys
 import cPickle
 from ConfigParser import SafeConfigParser
+from optparse import OptionParser
 
 import feedparser
 import tweepy
@@ -32,60 +33,132 @@ _LOGLEVEL = getattr(logging, os.getenv('LOGLEVEL', 'WARNING').upper())
 _LOGFORMAT = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 logging.basicConfig(level=_LOGLEVEL, format=_LOGFORMAT)
 
+def _parse_opts(argv=None):
+    """Parse the command line options.
 
-def post_update(status):
-    log = logging.getLogger('post_update')
-    log.debug('status: {}'.format(status))
+    :param list argv: List of arguments to process. If not provided then will
+        use optparse default
+    :return: options,args where options is the list of specified options that
+        were parsed and args is whatever arguments are left after parsing all
+        options.
+
+    """
+    parser = OptionParser(version='%prog {}'.format(__version__))
+    parser.set_defaults(verbose=False)
+    parser.add_option('-c', '--config', dest='config', metavar='FILE',
+        help='use config FILE (default: %default)', default='config.ini')
+    parser.add_option('-v', '--verbose', dest='verbose', action='store_true',
+        help='be more verbose (default is no)')
+    (options, args) = parser.parse_args(argv)
+    return options, args
+
+
+class TwitterBot(object):
+    def __init__(self, consumer_key, consumer_secret):
+        """Class to handle posting to twitter.
+
+        Intentionally the access_token and access_token_secret are not set at
+        initialization. This way it's possible to post different feeds to
+        different users.
+
+        :param string consumer_key: The application api key
+        :param string consumer_secret: The application api secret
+
+        """
+        self.log = logging.getLogger('main.TwitterBot')
+        self.consumer_key = consumer_key
+        self.consumer_secret = consumer_secret
+        self.access_token = None
+        self.access_token_secret = None
+
+    def post_update(self, status):
+        """Posts update to twitter.
+
+        :param string status: Status text to post
+        :return: Returns True on success
+
+        """
+        self.log.debug('enter post_update()')
+        self.log.debug('status: {}'.format(status))
+        auth = tweepy.OAuthHandler(self.consumer_key, self.consumer_secret)
+        auth.set_access_token(self.access_token, self.access_token_secret)
+        api = tweepy.API(auth)
+        try:
+            api.update_status(status)
+        except tweepy.error.TweepError as exc:
+            self.log.critical('Error occurred while updating status: {}'.format(exc))
+            sys.exit(1)
+        else:
+            return True
+
+    def process_feed(self, url, cache_file, suffix = '#xkcd'):
+        """Process the given feed for new items.
+
+        BUG: only posts the first item
+
+        :param string url: URL to RSS feed
+        :param string cache_file: Name of cache file
+        :param string suffix: Tags to append to status
+
+        """
+        self.log.debug('enter process_feed()')
+        self.log.debug('url: {}'.format(url))
+        self.log.debug('cache_file: {}'.format(cache_file))
+        self.log.debug('suffix: {}'.format(suffix))
+        feed = feedparser.parse(url)
+        try:
+            cache = cPickle.load(open(cache_file, 'rb'))
+        except IOError as exc:
+            if exc.errno == 2:
+                # 2 is file not found continue on
+                self.log.debug('Cache file not found, continue without cache')
+                cache = None
+            else:
+                raise
+        rss = {}
+        rss['id'] = feed['entries'][0]['id']
+        rss['link'] = feed['entries'][0]['link']
+        rss['title'] = feed['entries'][0]['title']
+        rss['summary'] = feed['entries'][0]['summary']
+        # compare with cache, if it exists
+        if not cache or cache['id'] != rss['id']:
+            post = '{} {} {}'.format(rss['title'], rss['link'], suffix)
+            self.log.info('new post: {}'.format(post))
+            self.post_update(post)
+            cPickle.dump(rss, open(cache_file, 'wb'), -1)
+
+
+def main(argv=None):
+    """The main function.
+
+    :param list argv: List of arguments passed to command line. Default is None,
+        which then will translate to having it set to sys.argv.
+
+    :return: Optionally returns a numeric exit code. If not given then will
+        default to 0.
+    :rtype: int
+
+    """
+    log = logging.getLogger('main')
+    if argv is None:
+        argv = sys.argv
+    options = _parse_opts(argv)[0]
+    if options.verbose:
+        log.setLevel(logging.DEBUG)
     config = SafeConfigParser()
-    if not config.read('config.ini'):
+    if not config.read(options.config):
         log.critical('Could not read config file')
         sys.exit(1)
     consumer_key = config.get('twitter', 'consumer_key')
     consumer_secret = config.get('twitter', 'consumer_secret')
     access_token = config.get('twitter', 'access_token')
     access_token_secret = config.get('twitter', 'access_token_secret')
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret, secure=True)
-    auth.set_access_token(access_token, access_token_secret)
-    api = tweepy.API(auth)
-    try:
-        api.update_status(status)
-    except tweepy.error.TweepError as exc:
-        log.critical('Error occurred while updating status: {}'.format(exc))
-        sys.exit(1)
-    else:
-        return True
-
-
-def process_feed(url, cache_file, suffix = '#xkcd'):
-    log = logging.getLogger('process_feed')
-    log.debug('url: {}'.format(url))
-    log.debug('cache_file: {}'.format(cache_file))
-    log.debug('suffix: {}'.format(suffix))
-    feed = feedparser.parse(url)
-    # lots of scary warnings about possible security risk using this method
-    # but for local use I'd rather do this than a try-catch with open()
-    if not os.path.isfile(cache_file):
-        # make a blank cache file
-        cPickle.dump({'id': None}, open(cache_file, 'wb'), -1)
-    cache = cPickle.load(open(cache_file))
-    rss = {}
-    rss['id'] = feed['entries'][0]['id']
-    rss['link'] = feed['entries'][0]['link']
-    rss['title'] = feed['entries'][0]['title']
-    rss['summary'] = feed['entries'][0]['summary']
-    # compare with cache
-    if cache['id'] != rss['id']:
-        post = '{} {} {}'.format(rss['title'], rss['link'], suffix)
-        log.info('new post: {}'.format(post))
-        post_update(post)
-        cPickle.dump(rss, open(cache_file, 'wb'), -1)
-
-
-def main():
-    """The main function."""
-    process_feed('http://xkcd.com/rss.xml', 'cache-xkcd.dat', '#xkcd')
-    process_feed('http://what-if.xkcd.com/feed.atom', 'cache-whatif.dat',
-        '#xkcd #whatif')
+    twitterbot = TwitterBot(consumer_key, consumer_secret)
+    twitterbot.access_token = access_token
+    twitterbot.access_token_secret = access_token_secret
+    twitterbot.process_feed('http://xkcd.com/rss.xml', 'cache-xkcd.dat', '#xkcd')
+    twitterbot.process_feed('http://what-if.xkcd.com/feed.atom',
+            'cache-whatif.dat', '#xkcd #whatif')
 
 
 if __name__ == "__main__":
