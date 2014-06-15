ae7e937f56019ec6652a6ea49ce4266db1ff4e3c
Todd Eddy <vr@vrillusions.com>
2014-06-15 00:45:30 -0400

    Set up travis-ci integration

---
+++ tests/update_twitter_test.py
@@ -0,0 +1,13 @@
+# vim: set fileencoding=utf-8 :
+"""Test update_twitter.py.
+
+"""
+
+from __future__ import (division, absolute_import, print_function,
+                        unicode_literals)
+
+# import update_twitter
+
+
+def initial_test():
+    pass
