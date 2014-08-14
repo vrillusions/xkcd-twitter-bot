462c4f4d1324196a0f4d9cfa5cc71e8710042a35
Todd Eddy <vr@vrillusions.com>
2014-08-14 01:48:31 -0400

    Add fabfile to automate tests and such

---
+++ fabfile.py
@@ -0,0 +1,14 @@
+# vim:fileencoding=utf-8
+from __future__ import absolute_import, print_function, unicode_literals
+
+from fabric.api import env, task, local
+
+
+env.use_ssh_config = True
+
+
+@task
+def test():
+    """runs through all tests"""
+    local("flake8")
+    local("nosetests")
